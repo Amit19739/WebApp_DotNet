6c5ffcd9ccb90def13976602427e21ad4abb0c43
Amit <amit19739@gmail.com>
2025-07-03 15:59:08 +0530

    Init Project

---
+++ WebApplication1/Controllers/TodoController.cs
@@ -0,0 +1,77 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[AllowAnonymous]
+[ApiController]
+public class TodoController(IConfiguration configuration) : ControllerBase
+{
+    private readonly string? _connectionString = configuration.GetConnectionString("mydb");
+
+    [HttpGet("GetTasks")]
+    public async Task<IActionResult> GetTasks()
+    {
+        var tasks = new List<object>(); // You can create a specific model instead of object
+        const string query = $"SELECT * FROM todo";
+
+        await using var con = new SqlConnection(_connectionString);
+        await using var cmd = new SqlCommand(query, con);
+        await con.OpenAsync();
+        await using var reader = await cmd.ExecuteReaderAsync();
+
+        while (await reader.ReadAsync())
+        {
+            tasks.Add(new
+            {
+                Id = reader["id"],
+                Task = reader["task"]
+            });
+        }
+        Console.WriteLine("GetAllTasks");
+        return Ok(tasks);
+    }
+
+    [HttpPost("AddTask")]
+    public async Task<IActionResult> AddTask([FromForm] string task)
+    {
+        const string query = "INSERT INTO todo (task) VALUES (@task)";
+
+        await using var con = new SqlConnection(_connectionString);
+        await using var cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@task", task);
+        await con.OpenAsync();
+        await cmd.ExecuteNonQueryAsync();
+        Console.WriteLine("Added Successfully");
+        return Ok(new JsonResult("Added Successfully"));
+    }
+
+    [HttpPost("DeleteTask")]
+    public async Task<IActionResult> DeleteTask([FromForm] int id)
+    {
+        const string query = "DELETE FROM todo WHERE id = @id";
+
+        await using var con = new SqlConnection(_connectionString);
+        await using var cmd = new SqlCommand(query, con);
+        cmd.Parameters.AddWithValue("@id", id);
+        await con.OpenAsync();
+        await cmd.ExecuteNonQueryAsync();
+        Console.WriteLine("Deleted Successfully");
+        return Ok(new JsonResult("Deleted Successfully"));
+    }
+    
+    [HttpPost("DeleteAllTasks")]
+    public async Task<IActionResult> DeleteAllTasks()
+    {
+        const string query = "DELETE FROM todo";
+
+        await using var con = new SqlConnection(_connectionString);
+        await using var cmd = new SqlCommand(query, con);
+        await con.OpenAsync();
+        int rowsAffected = await cmd.ExecuteNonQueryAsync();
+
+        return Ok(new JsonResult($"{rowsAffected} task(s) deleted successfully."));
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
+builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularClient", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") // Your Angular dev server URL
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+var app = builder.Build();
+//app.UseCors(x => x.AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowAnyMethod().AllowCredentials());
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors("AllowAngularClient");
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+app.MapControllers();
+// app.MapGet("/weatherforecast", () =>
+//     {
+//         var forecast = Enumerable.Range(1, 5).Select(index =>
+//                 new WeatherForecast
+//                 (
+//                     DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//                     Random.Shared.Next(-20, 55),
+//                     summaries[Random.Shared.Next(summaries.Length)]
+//                 ))
+//             .ToArray();
+//         return forecast;
+//     })
+//     .WithName("GetWeatherForecast")
+//     .WithOpenApi();
+//
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
